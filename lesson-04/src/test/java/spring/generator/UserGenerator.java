a1f3a600bd3cad34f5153733bcd81991346fadb2
AKaporov <voropak@yandex.ru>
2021-05-06 14:06:23 +0300

    Скорректировал файлы

--- lesson-04/src/test/java/spring/generator/UserGenerator.java
+++
@@ -1,9 +0,0 @@
-package spring.generator;
-
-import ru.otus.spring.domain.Student;
-
-public class UserGenerator {
-    public static Student createUser(String name, String surname){
-        return new Student(name, surname);
-    }
-}
