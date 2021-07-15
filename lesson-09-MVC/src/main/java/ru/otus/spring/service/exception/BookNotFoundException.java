117a1d3ab6360f02e6885ef8225ecb65baec9765
AKaporov <55753565+AKaporov@users.noreply.github.com>
2021-07-15 21:00:50 +0300

    update BookNotFoundException.java

--- lesson-09-MVC/src/main/java/ru/otus/spring/service/exception/BookNotFoundException.java
+++ lesson-09-MVC/src/main/java/ru/otus/spring/service/exception/BookNotFoundException.java
@@ -7,8 +7,4 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BookNotFoundException extends RuntimeException {
     private final String errorMessage;
-
-    public String getFullTextError() {
-        return String.format("The book {%s} was not found. Check the request details", this.errorMessage);
-    }
 }
